cfbc1b3231e56afda9c703c7fe1a81efbe8f57da
jilsonjoy98 <jilsonjoy98@gmail.com>
2022-12-21 17:19:32 +0530

    contact manager app v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UpdateContactComponent } from './update-contact/update-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // to redirect automatically from localhost:4200 to contactManager
+  {
+    path:'',redirectTo:'contacts/admim',pathMatch:'full'
+  },
+  // path for ContactManagerComponent -home
+  {
+    path:'contacts/admim',component:ContactManagerComponent
+  },
+  //AddContactComponent
+  {
+    path:'contacts/add', component:AddContactComponent
+  },
+  // UpdateContactComponent
+  {
+    path:'contacts/update/:contactId' ,component:UpdateContactComponent
+  },
+  //ViewContactComponent
+  {
+    path:'contacts/view/:contactId', component:ViewContactComponent
+  },
+  {
+    path:'**', component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MyContact } from "src/models/myContact";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseUrl:string ='http://localhost:3000/contacts'
+  constructor(private http:HttpClient) { }
+
+  //function  for get allcontacts
+  getAllcontacts():Observable<MyContact>{
+    return this.http.get(this.baseUrl)
+  }
+
+
+  //function for view a particular contact
+ViewContact(contactId:string){
+   return this.http.get(`${this.baseUrl}/${contactId}`)
+  }
+
+  // function getting a particular groupname
+  getGroupName(groupId:string){
+    return this.http.get('http://localhost:3000/groups/'+groupId)
+  }
+
+  // fuction for fetch all groups from http://localhost:3000/groups
+  getAllGroups(){
+    return this.http.get('http://localhost:3000/groups')
+  }
+
+  // fuction for adding new contact to  server
+addContact(contactBody:any){
+ return this.http.post(this.baseUrl,contactBody)
+
+}
+
+}
+
+
